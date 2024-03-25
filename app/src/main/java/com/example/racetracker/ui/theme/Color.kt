f1a149c35d636e1ec84284978929daf75ae270cb
Roumaysae <roumaysaeelamrani2002@gmail.com>
2024-03-25 16:34:23 +0000

    Initial commit

---
+++ app/src/main/java/com/example/racetracker/ui/theme/Color.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.racetracker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFFD81B60)
+val md_theme_light_onPrimary = Color(0xFFC79DE0)
+val md_theme_light_primaryContainer = Color(0xFFEC407A)
+val md_theme_light_onPrimaryContainer = Color(0xFF351000)
+val md_theme_light_secondary = Color(0xFF7B1FA2)
+val md_theme_light_onSecondary = Color(0xFF888585)
+val md_theme_light_secondaryContainer = Color(0xFFFFDBCC)
+val md_theme_light_onSecondaryContainer = Color(0xFF2C160C)
+val md_theme_light_tertiary = Color(0xFFBA68C8)
+val md_theme_light_onTertiary = Color(0xFFFFC107)
+val md_theme_light_tertiaryContainer = Color(0xFFEEE4A9)
+val md_theme_light_onTertiaryContainer = Color(0xFFFFEE58)
+val md_theme_light_error = Color(0xFFE57373)
+val md_theme_light_errorContainer = Color(0xFFFFCDD2)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFF5F5F5)
+val md_theme_light_onBackground = Color(0xFF201A18)
+val md_theme_light_surface = Color(0xFFBDBDBD)
+val md_theme_light_onSurface = Color(0xFF201A18)
+val md_theme_light_surfaceVariant = Color(0xFFF4DED5)
+val md_theme_light_onSurfaceVariant = Color(0xFF52443D)
+val md_theme_light_outline = Color(0xFF757575)
+val md_theme_light_inverseOnSurface = Color(0xFFFBEEE9)
+val md_theme_light_inverseSurface = Color(0xFF362F2C)
+val md_theme_light_inversePrimary = Color(0xFFEF9A9A)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFFFFF176)
+val md_theme_light_outlineVariant = Color(0xFF9E9E9E)
+val md_theme_light_scrim = Color(0xFF000000)
+
+val md_theme_dark_primary = Color(0xFFD81B60)
+val md_theme_dark_onPrimary = Color(0xFF571F00)
+val md_theme_dark_primaryContainer = Color(0xFFEC407A)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFDBCC)
+val md_theme_dark_secondary = Color(0xFF7B1FA2)
+val md_theme_dark_onSecondary = Color(0xFFBA68C8)
+val md_theme_dark_secondaryContainer = Color(0xFF5D4034)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFDBCC)
+val md_theme_dark_tertiary = Color(0xFFFFC107)
+val md_theme_dark_onTertiary = Color(0xFF363106)
+val md_theme_dark_tertiaryContainer = Color(0xFFFFEE58)
+val md_theme_dark_onTertiaryContainer = Color(0xFFEEE4A9)
+val md_theme_dark_error = Color(0xFFE57373)
+val md_theme_dark_errorContainer = Color(0xFFFFCDD2)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF121212)
+val md_theme_dark_onBackground = Color(0xFFEDE0DB)
+val md_theme_dark_surface = Color(0xFF212121)
+val md_theme_dark_onSurface = Color(0xFFEDE0DB)
+val md_theme_dark_surfaceVariant = Color(0xFF424242)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD8C2BA)
+val md_theme_dark_outline = Color(0xFF757575)
+val md_theme_dark_inverseOnSurface = Color(0xFF201A18)
+val md_theme_dark_inverseSurface = Color(0xFFEDE0DB)
+val md_theme_dark_inversePrimary = Color(0xFFEF9A9A)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFFFF176)
+val md_theme_dark_outlineVariant = Color(0xFF9E9E9E)
+val md_theme_dark_scrim = Color(0xFF000000)
